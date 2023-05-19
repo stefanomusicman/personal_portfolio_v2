a6a1f3161bd3d5aa21481deb63b50e7f00078842
stefano <stef.pro@hotmail.com>
2023-05-19 15:22:46 -0400

    beginning to build hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import NavBar from "./components/NavBar";
+import HeroSection from "@/components/HeroSection";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
-    <main className="w-11/12 md:w-85/100 lg:w-3/4">
+    <main className="w-full flex flex-col items-center h-screen md:w-85/100 lg:w-3/4">
       <NavBar />
+      <HeroSection />
     </main>
   )
 }
