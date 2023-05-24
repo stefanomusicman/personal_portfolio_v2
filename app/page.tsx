b023ad49e66e473331062c0d8b0668f2ff7d244e
stefano <stef.pro@hotmail.com>
2023-05-24 17:37:54 -0400

    adding projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import NavBar from "../components/NavBar";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <main className="w-full flex flex-col items-center h-screen md:w-85/100 lg:w-3/4">
+    <main className="w-full flex flex-col items-center md:w-10/12 lg:w-3/4">
       <NavBar />
       <HeroSection />
+      <Projects />
     </main>
   )
 }
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
     return (
-        <div className="w-full h-[calc(100vh-96px)] relative flex flex-col items-center lg:h-[calc(100vh-96px)] lg:flex-row lg:justify-start lg:items-center">
+        <div className="w-full h-[calc(100vh-96px)] relative flex flex-col items-center lg:flex-row lg:justify-start lg:items-center">
             {/* Circle 1 */}
             <div className="
                 -left-16 
@@ -12,11 +12,16 @@ const HeroSection = () => {
                 bg-gradient-to-r from-blue-500 to-green-500 
                 p-px 
                 absolute 
-                lg:w-[40rem] 
-                lg:h-[40rem] 
-                lg:left-auto 
-                lg:bottom-20 
-                lg:right-32 
+                lg:w-[25rem]
+                lg:h-[25rem]
+                lg:bottom-auto
+                lg:left-auto
+                lg:right-10
+                xl:w-[40rem] 
+                xl:h-[40rem] 
+                xl:left-auto 
+                xl:bottom-20 
+                xl:right-32 
                 rounded-full">
                 <div className="h-full w-full bg-[#060b1b] rounded-full"></div>
             </div>
@@ -30,12 +35,17 @@ const HeroSection = () => {
                 p-px 
                 absolute 
                 shadow-lg shadow-cyan-500/50
-                lg:w-[15rem] 
-                lg:h-[15rem] 
-                lg:left-auto 
-                lg:left-100 
-                lg:bottom-80 
-                lg:right-0 
+                lg:w-[10rem]
+                lg:h-[10rem]
+                lg:left-auto
+                lg:bottom-auto
+                lg:right-0
+                xl:w-[15rem] 
+                xl:h-[15rem] 
+                xl:left-auto 
+                xl:left-100 
+                xl:bottom-80 
+                xl:right-0 
                 rounded-full">
             </div>
             {/* Circle 3 */}
@@ -48,12 +58,18 @@ const HeroSection = () => {
                 p-px 
                 absolute 
                 shadow-lg shadow-cyan-500/50
-                lg:w-[20rem] 
-                lg:h-[20rem] 
-                lg:left-auto 
-                lg:left-100 
-                lg:bottom-[28rem] 
-                lg:right-24 
+                lg:w-[12rem]
+                lg:h-[12rem]
+                lg:left-auto
+                lg:bottom-auto
+                lg:right-10
+                lg:bottom-[29rem]
+                xl:w-[20rem] 
+                xl:h-[20rem] 
+                xl:left-auto 
+                xl:left-100 
+                xl:bottom-[28rem] 
+                xl:right-24 
                 rounded-full">
             </div>
 
@@ -65,7 +81,7 @@ const HeroSection = () => {
                 flex
                 flex-col
                 justify-evenly
-                h-2/5
+                h-1/2
                 lg:justify-center
                 lg:w-4/6 
                 lg:h-[calc(100vh-96px)]">
@@ -75,8 +91,8 @@ const HeroSection = () => {
                     lg:flex
                     lg:flex-col
                     lg:justify-evenly">
-                    <h1 className="font-bold text-2xl lg:text-6xl bg-gradient-to-r from-blue-500 to-green-500 inline-block text-transparent bg-clip-text">Hello, I'm Stefano,</h1>
-                    <h1 className="text-2xl lg:text-6xl">software developer</h1>
+                    <h1 className="font-bold text-2xl lg:text-5xl xl:text-6xl bg-gradient-to-r from-blue-500 to-green-500 inline-block text-transparent bg-clip-text">Hello, I'm Stefano,</h1>
+                    <h1 className="text-2xl lg:text-5xl xl:text-6xl">software developer</h1>
                 </div>
                 {/* description container */}
                 <div className="
@@ -98,8 +114,9 @@ const HeroSection = () => {
                     bg-gradient-to-r from-blue-500 to-green-500
                     px-4
                     py-1
-                    lg:w-1/5
-                    lg:py-3
+                    lg:w-1/4
+                    xl:w-1/5
+                    xl:py-3
                     ">
                         Download CV
                     </button>
