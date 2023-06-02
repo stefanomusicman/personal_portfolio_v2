17aa90f6071d49d6b82a101edc56a9ab71567129
stefano <stef.pro@hotmail.com>
2023-06-02 16:37:47 -0400

    implemented solution for FOUC

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+'use client';
 import HeroSection from "@/components/HeroSection";
 import NavBar from "../components/NavBar";
 import Projects from "@/components/Projects";
 import Tech from "@/components/Tech";
 import AboutMe from "@/components/AboutMe";
+import { gsap } from "gsap";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  // basic solution to deal with FOUC
+  useEffect(() => {
+    gsap.set('body', { opacity: 1 })
+    gsap.from('body', { opacity: 0 })
+  }, [])
+
   return (
     <main className="w-full flex flex-col items-center md:w-10/12 lg:w-3/4">
       <NavBar />
